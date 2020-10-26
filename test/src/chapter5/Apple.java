ce3d1a438c5d7f13829f191b8b48335e1ad1cb95
lancibe <1609547089@qq.com>
2020-10-26 13:19:17 +0800

    Apple

---
+++ test/src/chapter5/Apple.java
@@ -0,0 +1,20 @@
+public class Apple {
+    public String name;
+    public String color;
+    public double weight;
+    public Apple(){}
+    public Apple(String name, String color)
+    {
+        this.name = name;
+        this.color = color;
+    }
+
+    public Apple(String name, String color, double weight)
+    {
+        // 通过this调用另一个重载的构造器的初始化代码
+        this(name, color);
+        //下面this引用该构造器正在初始化的Java对象
+        this.weight = weight;
+
+    }
+}
