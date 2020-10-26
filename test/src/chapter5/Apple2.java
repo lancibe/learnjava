ade0560d507c72ae627d03eb3459458abe9ca28c
lancibe <1609547089@qq.com>
2020-10-26 13:25:42 +0800

    Fruit

---
+++ test/src/chapter5/Apple2.java
@@ -0,0 +1,11 @@
+package fruit;
+
+public class Apple2 extends fruit.Fruit {
+    public static void main(String[] args)
+    {
+        Apple2 a = new Apple2();
+
+        a.weight = 56;
+        a.info();
+    }
+}
---
+++ test/src/chapter5/Fruit.java
@@ -0,0 +1,9 @@
+package fruit;
+
+public class Fruit {
+    public double weight;
+    public void info()
+    {
+        System.out.println("fruit"+weight);
+    }
+}
