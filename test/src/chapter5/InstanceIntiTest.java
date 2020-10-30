67a69305a61fdc45fe25ba7f569686cc956910d3
lancibe <1609547089@qq.com>
2020-10-30 09:32:45 +0800

    instance init test

---
+++ test/src/chapter5/InstanceIntiTest.java
@@ -0,0 +1,13 @@
+public class InstanceIntiTest {
+    //先执行初始化块将a实例变量赋值为6
+    {
+        a=6;
+    }
+    //再执行将a的实例变量赋值为9
+    int a =9;
+    public static void main(String[] args)
+    {
+        //下面代码将输出9
+        System.out.println(new InstanceIntiTest().a);
+    }
+}
