9ca4860e368b68e5f69d8bbb57071dbc12d353d2
lancibe <1609547089@qq.com>
2020-10-30 09:19:36 +0800

    person

---
+++ test/src/chapter5/Person/Person.java
@@ -0,0 +1,27 @@
+package chapter5.Person;
+
+public class Person {
+    //下面定义一个初始化块
+    {
+        int a = 6;
+        if( a > 4 )
+        {
+            System.out.println("Person初始化块：局部变量a的值大于4");
+        }
+        System.out.println("Person的初始化块");
+    }
+
+    //定义第二个初始化块
+    {
+        System.out.println("Person的第二个初始化块");
+    }
+
+    public Person()
+    {
+        System.out.println("Person类的无参数构造器");
+    }
+    public static void main(String[] args)
+    {
+        new Person();
+    }
+}
