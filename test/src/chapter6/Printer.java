50a8d45b8f685cf2689d0f89c23eecc64fa14920
lancibe <1609547089@qq.com>
2020-11-12 22:11:50 +0800

    printer

---
+++ test/src/chapter6/Printer.java
@@ -0,0 +1,58 @@
+//定义一个Product接口
+interface Product
+{
+    int getProduceTime();
+}
+//让printer类实现Output和Product接口
+public class Printer implements Output, Product {
+    private String[] printData = new String[Output.MAX_CHCHE_LINE];
+    //用以记录当前需打印的作业数
+    private int dataNum = 0;
+    public void out()
+    {
+        //只要还有作业，就继续打印
+        while(dataNum > 0)
+        {
+            System.out.println("打印机打印："+printData[0]);
+            //把作业队列整体前移一位，并将剩下的作业数减一
+            System.arraycopy(printData, 1, printData, 0, --dataNum);
+        }
+    }
+    public void getData(String msg)
+    {
+        if(dataNum >= Output.MAX_CHCHE_LINE)
+        {
+            System.out.println("输出队列已满，添加失败");
+        }
+        else
+        {
+            //把打印数据添加到队列里，已保存数据的数量加一
+            printData[dataNum++] = msg;
+        }
+    }
+
+    @Override
+    public int getProduceTime() {
+        return 45;
+    }
+    public static void main(String[] args)
+    {
+        //创建一个Printer对象，当成Output使用
+        Output o = new Printer();
+        o.getData("lancibe");
+        o.getData("babyxun");
+        o.out();
+        o.getData("Fei");
+        o.getData("babyfei");
+        o.out();
+        //调用Output接口中定义的默认方法
+        o.print("first", "second", "third");
+        o.test();
+        //创建一个Printer对象，当成Product使用
+        Product p = new Printer();
+        System.out.println(p.getProduceTime());
+
+        //所有接口类型的引用变量都可以直接赋给Object类型的变量
+        Object obj = p;
+    }
+}
