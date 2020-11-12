b0eec161274d7129a0dace3541ba8a6b68857328
lancibe <1609547089@qq.com>
2020-11-12 21:36:05 +0800

    output field test

---
+++ test/src/chapter6/OutputFieldTest.java
@@ -0,0 +1,7 @@
+public class OutputFieldTest {
+    public static void main(String[] args)
+    {
+        System.out.println(Output.MAX_CHCHE_LINE);
+        System.out.println(Output.staticTest());
+    }
+}
