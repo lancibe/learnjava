acede92d51aad12291b7088fb78dcd1c05637d21
lancibe <1609547089@qq.com>
2020-11-13 10:36:54 +0800

    add command

---
+++ test/src/chapter6/AddCommand.java
@@ -0,0 +1,11 @@
+public class AddCommand implements Command {
+    public void process(int[] target)
+    {
+        int sum = 0;
+        for(int tmp:target)
+        {
+            sum += tmp;
+        }
+        System.out.println("数组元素总和是："+sum);
+    }
+}
--- test/src/chapter6/CommandTest.java
+++ test/src/chapter6/CommandTest.java
@@ -1,2 +1,11 @@
-package chapter6;public class CommandTest {
+public class CommandTest {
+    public static void main(String[] args)
+    {
+        ProcessArray pa = new ProcessArray();
+        int[] target = {3, -4, 6, 4};
+        //第一次处理数据，具体处理行为取决于PrintCommand
+        pa.process(target, new PrintCommand());
+        System.out.println("---------------");
+        pa.process(target, new AddCommand());
+    }
 }
