6dfcb659f84df7c27983de32906829122f81bf38
lancibe <1609547089@qq.com>
2020-11-15 20:12:12 +0800

    command test

--- test/src/chapter6/CommandTest.java
+++ test/src/chapter6/CommandTest.java
@@ -1,15 +1,19 @@
-import ProcessArray;
-import PrintCommand;
-import AddCommand;
-
 public class CommandTest {
     public static void main(String[] args)
     {
         ProcessArray pa = new ProcessArray();
         int[] target = {3, -4, 6, 4};
-        //第一次处理数据，具体处理行为取决于PrintCommand
-        pa.process(target, new PrintCommand());
-        System.out.println("---------------");
-        pa.process(target, new AddCommand());
+        //处理数组，具体处理行为取决于匿名内部类
+        pa.process(target, new Command() {
+            public void process(int[] target)
+            {
+                int sum = 0;
+                for(int tmp:target)
+                {
+                    sum += tmp;
+                }
+                System.out.println("数组元素的总和是："+sum);
+            }
+        });
     }
 }
