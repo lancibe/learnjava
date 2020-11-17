049879e143cfa275be459d612185ecdaca156ad9
lancibe <1609547089@qq.com>
2020-11-17 13:05:36 +0800

    lambda test

---
+++ test/src/chapter6/LambdaTest.java
@@ -0,0 +1,14 @@
+public class LambdaTest {
+    Runnable r = ()->{
+        for (int i = 0 ; i < 100 ; i++)
+        {
+            System.out.println();
+        }
+    };
+    Object obj1 = (Runnable)()->{
+        for (int i = 0 ; i < 100 ; i++)
+        {
+            System.out.println();
+        }
+    };
+}
