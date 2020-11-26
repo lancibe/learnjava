035ced91ce112dea799fad17c74325879fc28c46
lancibe <1609547089@qq.com>
2020-11-26 21:02:50 +0800

    string reg

---
+++ test/src/chapter7/StringReg.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class StringReg {
+    public static void main(String[] args)
+    {
+        String[] msgs =
+                {
+                        "Java has regular expressions in 1.4",
+                        "regular expressions now expressing is Java",
+                        "Java represses oracular expressions"
+                };
+        for(String msg:msgs)
+        {
+            System.out.println(msg.replaceFirst("re\\w*", "哈哈:)"));
+            System.out.println(Arrays.toString(msg.split(" ")));
+        }
+    }
+}
