72171f79e32e74944a13fe94f3753f021584d062
lancibe <1609547089@qq.com>
2020-12-01 11:48:11 +0800

    tree set test

---
+++ test/src/chapter8/TreeSetTest.java
@@ -0,0 +1,19 @@
+import java.util.TreeSet;
+
+public class TreeSetTest {
+    public static void main(String[] args)
+    {
+        TreeSet nums = new TreeSet();
+        nums.add(5);
+        nums.add(2);
+        nums.add(10);
+        nums.add(-9);
+
+        System.out.println(nums);
+        System.out.println(nums.first());
+        System.out.println(nums.last());
+        System.out.println(nums.headSet(4));
+        System.out.println(nums.tailSet(5));
+        System.out.println(nums.subSet(-3,4));
+    }
+}
