aa8a45644605130523cd1e5bf26c2ae9c948e169
lancibe <1609547089@qq.com>
2020-12-03 16:07:45 +0800

    tree set error 2

---
+++ test/src/chapter8/TreeSetError2.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+import java.util.TreeSet;
+
+public class TreeSetError2 {
+    public static void main(String[] args)
+    {
+        TreeSet ts = new TreeSet();
+        ts.add(new Date());
+        //ts.add(new String("lancibe"));
+
+    }
+}
