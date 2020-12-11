4a7a4c0f4e63cbdfb434c653389b90800461d426
lancibe <1609547089@qq.com>
2020-12-11 21:36:27 +0800

    sort test

--- test/src/chapter8/SortTest.java
+++ test/src/chapter8/SortTest.java
@@ -1,4 +1,20 @@
-package chapter8;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class SortTest {
+    public static void main(String[] args)
+    {
+        ArrayList nums = new ArrayList();
+        nums.add(2);
+        nums.add(-5);
+        nums.add(3);
+        nums.add(0);
+        System.out.println(nums);
+        Collections.reverse(nums);
+        System.out.println(nums);
+        Collections.sort(nums);
+        System.out.println(nums);
+        Collections.shuffle(nums);
+        System.out.println(nums);
+    }
 }
