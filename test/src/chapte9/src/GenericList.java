53fa78490548cfbf096268484037edbf3dcfebb3
lancibe <1609547089@qq.com>
2020-12-28 10:47:13 +0800

    generic list

--- test/src/chapte9/src/GenericList.java
+++ test/src/chapte9/src/GenericList.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenericList {
+    public static void main(String args[])
+    {
+        //创建一个只想保存字符串的List集合
+        List<String> strList = new ArrayList<>();
+        strList.add("lancibe");
+        strList.add("xun");
+        //下面代码直接引起编译错误
+        strList.add(5);
+        strList.forEach(str -> System.out.println(str.length()));
+    }
 }
