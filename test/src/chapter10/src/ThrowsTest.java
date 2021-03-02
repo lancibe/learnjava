8c147f7ed7904225cb005815677dd328e6d771d8
lancibe <1609547089@qq.com>
2021-03-02 17:14:30 +0800

    throws test

---
+++ test/src/chapter10/src/ThrowsTest.java
@@ -0,0 +1,10 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ThrowsTest {
+    public static void main(String[] args)
+        throws IOException
+    {
+        FileInputStream fis = new FileInputStream("b.txt");
+    }
+}
