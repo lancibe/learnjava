07050eb64706bad33b942b8a5bcfe558fe3324bd
lancibe <1609547089@qq.com>
2021-03-02 17:49:04 +0800

    Auction exception

---
+++ test/src/chapter10/src/AuctionException.java
@@ -0,0 +1,9 @@
+public class AuctionException extends Exception{
+    //无参数构造器
+    public AuctionException(){}
+    //带一个字符串参数的构造器
+    public AuctionException(String msg)
+    {
+        super(msg);
+    }
+}
