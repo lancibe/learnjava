f45d9c2964898b8afad383b24a10f3159d56086c
lancibe <1609547089@qq.com>
2021-03-06 11:57:03 +0800

    box space test

---
+++ test/src/chapter11/src/BoxSpaceTest.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class BoxSpaceTest {
+    private Frame f = new Frame("测试");
+    private Box horizontal = Box.createHorizontalBox();
+    private Box vertical = Box.createVerticalBox();
+    public void init()
+    {
+        horizontal.add(new Button("水平按钮一"));
+        horizontal.add(Box.createHorizontalGlue());
+        horizontal.add(Box.createHorizontalStrut(10));
+        horizontal.add(new Button("水平按钮二"));
+        vertical.add(new Button("垂直按钮一"));
+        vertical.add(Box.createVerticalGlue());
+        vertical.add(new Button("垂直按钮二"));
+        vertical.add(Box.createVerticalStrut(10));
+        vertical.add(new Button("垂直按钮三"));
+        f.add(horizontal, BorderLayout.NORTH);
+        f.add(vertical);
+        f.pack();
+        f.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new BoxSpaceTest().init();
+    }
+}
