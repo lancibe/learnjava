25c87c4865150570fabd2dda255741e6d5803f91
lancibe <1609547089@qq.com>
2021-03-08 18:25:04 +0800

    zoom image

---
+++ test/src/chapter11/src/ZoomImage.java
@@ -0,0 +1,25 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class ZoomImage {
+    private final int WIDTH = 80;
+    private final int HEIGHT = 60;
+    //定义一个BufferedImage对象，用于保存缩小之后的位图
+    BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+    Graphics g = image.getGraphics();
+    public void zoom()
+            throws Exception
+    {
+        Image srcImage = ImageIO.read(new File("image/board.jpg"));
+        g.drawImage(srcImage, 0, 0, WIDTH, HEIGHT, null);
+        ImageIO.write(image, "jpeg", new File(System.currentTimeMillis() + ".jpg"));
+    }
+
+    public static void main(String[] args)
+            throws Exception
+    {
+        new ZoomImage().zoom();
+    }
+}
