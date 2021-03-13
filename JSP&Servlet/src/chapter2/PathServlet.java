2d88a9366aff297d0317dceb2b562ea1475151f4
lancibe <1609547089@qq.com>
2021-03-13 16:33:07 +0800

    path servlet

---
+++ JSP&Servlet/src/chapter2/PathServlet.java
@@ -0,0 +1,30 @@
+package chapter2;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/servlet/*")
+public class PathServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Servlet Pattern</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println(req.getRequestURI() + "<br>");
+        out.println(req.getContextPath() + "<br>");
+        out.println(req.getServletPath() + "<br>");
+        out.println(req.getPathInfo());
+        out.println("</body>");
+        out.println("</html>");
+        out.close();
+    }
+}
