7378c80d5dea08822988c8c245cd5ce30c660174
lancibe <1609547089@qq.com>
2021-03-22 15:57:41 +0800

    questionnaire

--- JSP&Servlet/src/chapter4/Questionnaire.java
+++ JSP&Servlet/src/chapter4/Questionnaire.java
@@ -5,6 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -32,15 +33,18 @@ public class Questionnaire extends HttpServlet {
         {
             String p1q1 = request.getParameter("p1q1");
             String p1q2 = request.getParameter("p1q2");
+            HttpSession session = request.getSession();
+            session.setAttribute("p1q1", p1q1);
+            session.setAttribute("p1q2", p1q2);
             out.println("问题三：<input type='text' name='p2q1'><br>");
-            out.println("<input type='hidden' name='p1q1' value='" + p1q1 +"'>");
-            out.println("<input type='hidden' name='p1q2' value='" + p1q2 +"'>");
             out.println("<input type='submit' name='page' value='完成'>");
         }
         else if("完成".equals(page))
         {
-            out.println(request.getParameter("p1q1")+"<br>");
-            out.println(request.getParameter("p1q2")+"<br>");
+            HttpSession session = request.getSession();
+            session.getAttribute("p1q1");
+            out.println(session.getAttribute("p1q1")+"<br>");
+            out.println(session.getAttribute("p1q2")+"<br>");
             out.println(request.getParameter("p2q1")+"<br>");
         }
         out.println("</form>");
