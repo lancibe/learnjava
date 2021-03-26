e96cc4c925735ff23d7cebf112f4d5955ca20b28
lancibe <1609547089@qq.com>
2021-03-26 16:16:30 +0800

    users

---
+++ JSP&Servlet/src/chapter5/SessionListenerDemo2Login.java
@@ -0,0 +1,35 @@
+package chapter5;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet("/SessionListenerDemo2Login.do")
+public class SessionListenerDemo2Login extends HttpServlet {
+    private Map<String, String> users;
+
+    public SessionListenerDemo2Login(){
+        users = new HashMap<String, String>();
+        users.put("lancibe", "123456");
+        users.put("lancibe22", "123456");
+        users.put("root", "1234567890");
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String passwd = req.getParameter("passwd");
+        String page = "form.html";
+        if(users.containsKey(name) && users.get(name).equals(passwd))
+        {
+            User user = new User(name);
+            req.getSession().setAttribute("user", user);
+            page = "SessionListenerDemoWelcome.view";
+        }
+        resp.sendRedirect(page);
+    }
+}
