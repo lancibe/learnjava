2ebdacf3b696a5c11dc9a8139513a1c84365997d
lancibe <1609547089@qq.com>
2021-03-31 17:14:32 +0800

    web innit listener

---
+++ JSP&Servlet/src/chapter5/WebInitListener.java
@@ -0,0 +1,47 @@
+package chapter5;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebListener()
+public class WebInitListener implements ServletContextListener {
+    private List<AsyncContext> asyncs = new ArrayList<AsyncContext>();
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        sce.getServletContext().setAttribute("asyncs", asyncs);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true)
+                {
+                    try {
+                        Thread.sleep((int) (Math.random() * 10000));
+                        double num = Math.random() * 10;
+                        synchronized (asyncs)
+                        {
+                            for(AsyncContext ctx : asyncs)
+                            {
+                                ctx.getResponse().getWriter().println(num);
+                                ctx.complete();
+                            }
+                            asyncs.clear();
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        throw new RuntimeException(e)
+                    }
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
+}
