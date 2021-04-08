8e9e3c4f54ecd3ca52a7ac30e5af4176907aac80
lancibe <1609547089@qq.com>
2021-04-08 11:15:04 +0800

    user

---
+++ JSP&Servlet/chapter6/src/User.java
@@ -0,0 +1,26 @@
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isValid(){
+        return "lancibe".equals(name) && "123456".equals(password);
+    }
+}
